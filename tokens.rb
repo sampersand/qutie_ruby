985b57f7ea7998802d2fecf6e56f211fbddd1d2b
westerhack <westerhack@gmail.com>
2017-02-02 18:45:35 -0600

    Woah, Stream is just a Universe without knowns. I think i'm going to encorperate that

--- tokens.rb
+++ tokens.rb
@@ -1,45 +1,27 @@
-
 require_relative 'stream'
-class Tokens < Array
-
-  attr_reader :parens
+class Universe
+  attr_reader :stack
   attr_reader :knowns
 
-  def initialize(body: nil, parens: nil, knowns: nil)
-    super()
-    concat body if body
-    @parens = parens || [nil, nil]
+  def initialize(stack: nil, knowns: nil)
+    @stack = stack || []
     @knowns = knowns || {}
   end
 
   def inspect
-    res = join.strip
-    res = "#{@parens[0]} #{res} #{@parens[1]}" unless @parens.none?
-    "[#{res}] <#{@knowns}>"
+    stack_str = @stack.join.strip
+    knowns_str = @knowns.join.strip
+    "<#{stack_str} | #{knowns_str}>"
   end
 
-  def to_stream
-    Stream.new(self + ["\n"])
+  def <<(val)
+    @stack << val
   end
 
-  def +(other)
-    self.class.new(body: super, knowns: @knowns.clone)
-  end
-  def clone
-    self.class.new(body: super, parens: @parens.clone, knowns: @knowns.clone)
+  alias :push :val
+
+  def pop
+    @stack.pop
   end
 
-  def clone_knowns
-    self.class.new(knowns: @knowns.clone)
-  end
-
-  def merge!(other)
-    raise unless other.is_a?(Tokens)
-    @knowns.update(other.knowns)
-    concat other
-  end
-
-  def merge(other)
-    clone.merge!(other)
-  end
 end
