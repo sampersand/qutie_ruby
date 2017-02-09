8e84bfa77c771dcd00ad81dfcfa08fd1b19273f4
westerhack <westerhack@gmail.com>
2017-02-08 23:09:16 -0600

    Fixed the problem with getting variables

--- ruby/constants.rb
+++ ruby/constants.rb
@@ -3,32 +3,24 @@ require_relative 'plugins/number'
 
 module Constants
   class QT_Boolean < QT_Object
-    class QT_Boolean_True < QT_Boolean
-
-    end
-
-    class QT_Boolean_False < QT_Boolean
-
-    end
-    class QT_Boolean_Null < QT_Boolean
-
-    end
-
     # consts
-      TRUE = QT_Boolean_True::from(source: 'true')
-      FALSE = QT_Boolean_False::from(source: 'false')
-      NULL = QT_Boolean_Null::from(source: 'null')
+      TRUE = QT_Boolean::from(source: 'true')
+      FALSE = QT_Boolean::from(source: 'false')
     def qt_to_text
       Text::QT_Text.new(text_val: to_s)
     end
   end
 
+  class QT_Null < QT_Object
+    NULL = QT_Null.new
+  end
+
   CONSTANTS = {
     true: QT_Boolean::TRUE,
     false: QT_Boolean::FALSE,
-    nil: QT_Boolean::NULL,
-    null: QT_Boolean::NULL,
-    none: QT_Boolean::NULL,
+    nil: QT_Null::NULL,
+    null: QT_Null::NULL,
+    none: QT_Null::NULL,
     math_e: Number::QT_Number::E,
     math_pi: Number::QT_Number::PI,
   }
