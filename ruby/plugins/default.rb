6bf6c63e87093d05c6f2c1aa143f43722e94b50f
westerhack <westerhack@gmail.com>
2017-02-08 20:22:15 -0600

    Added QT_Object and QT_Misc - maybe should rename that last one

--- ruby/plugins/default.rb
+++ ruby/plugins/default.rb
@@ -1,9 +1,16 @@
 module Default
+  require_relative 'QT_Object'
+  class QT_Misc
+    def initialize(object)
+      @object = object
+    end
+  end
+
   module_function
   def next_token!(stream, _, _)
     stream.next
   end
   def handle(token, _, universe, _)
-    universe << token
+    universe << QT_Misc.new(token)
   end
 end
---
+++ ruby/plugins/object.rb
@@ -0,0 +1,2 @@
+class QT_Object
+end
