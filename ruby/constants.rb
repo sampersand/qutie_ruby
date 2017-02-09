35ce721cbd7db1e128124e7de24b4786ff47830d
westerhack <westerhack@gmail.com>
2017-02-08 19:49:31 -0600

    I went thru and checked, I'm confident they all work

--- ruby/constants.rb
+++ ruby/constants.rb
@@ -7,19 +7,4 @@ module Constants
     null: nil,
     none: nil
   }
-
-  # def next_token!(stream, _, _)
-  #   res = BOOELEANS.find{ |_, sym| stream.peek?(sym) }
-  #   res and stream.next(amnt: res[-1])
-  # end
-
-  # def handle(token, stream, universe, parser)
-  #   universe << (case token.downcase
-  #                when BOOELEANS[:true] then true
-  #                when BOOELEANS[:false] then false
-  #                when BOOELEANS[:nil], BOOELEANS[:null], BOOELEANS[:none] then nil
-  #                else raise "Unknown boolean `#{token}`"
-  #                end)
-  # end
-
 end
