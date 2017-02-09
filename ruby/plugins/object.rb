cfe8221fa077a44fba846bdee1676497bb0d0279
westerhack <westerhack@gmail.com>
2017-02-08 22:10:20 -0600

    Adding in operator functions in more detail

--- ruby/plugins/object.rb
+++ ruby/plugins/object.rb
@@ -16,6 +16,7 @@ class QT_Object
     # conversion
       def qt_to_num;  end
       def qt_to_text; Text::QT_Text.new(text_val: to_s) end
+      # def qt_to_bool; Text::QT_Text.new(text_val: to_s) end
 
     # operators 
       # access
@@ -36,4 +37,37 @@ class QT_Object
         def qt_mod_r(left:); end
         def qt_pow_r(left:); end
 
+      # comparison
+        def qt_eql(right:); end
+        def qt_lth(right:); end
+        def qt_gth(right:); end
+
+        def qt_neq(right:)
+          eql = qt_eql(right: right)
+          eql && eql.qt_not
+        end
+        def qt_leq(right:)
+          lth = qt_lth(right: right)
+          lth || qt_eql(right: right)
+        end
+        def qt_geq(right:)
+          gth = qt_gth(right: right)
+          gth || qt_eql(right: right)
+        end
+
+        def qt_eql_r(left:); qt_eql(right: left) end
+        def qt_lth_r(left:); qt_gth(right: left) end
+        def qt_gth_r(left:); qt_lth(right: left) end
+        def qt_neq_r(left:); qt_neq(right: left) end
+        def qt_leq_r(left:); qt_geq(right: left) end
+        def qt_geq_r(left:); qt_leq(right: left) end
+
+      # logic
+        def qt_not; qt_to_bool.qt_not end
+
 end
+
+
+
+
+
