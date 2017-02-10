7639e9df209d3bb304d54cb7d18d40ae3f725911
westerhack <westerhack@gmail.com>
2017-02-10 11:52:08 -0600

    I am going to remvoe catch_EOF - it seems redudant rn and if it's still needed later i'll add it back in

--- ruby/parser.rb
+++ ruby/parser.rb
@@ -33,7 +33,6 @@ class Parser
 
   def catch_EOF(universe, &block)
     levels = catch(:EOF, &block) or return false
-
     if levels[0] > 0
       throw :EOF, [levels[0]-1, levels[1]]
     elsif levels[0] == 0
