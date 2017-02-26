891f3ed2c98a9341f4969f016883a12cdb853ef9
westerhack <westerhack@gmail.com>
2017-02-25 20:13:29 -0600

    You can now pass code blocks to default functions

--- functions.rb
+++ functions.rb
@@ -13,39 +13,62 @@ module Functions
 
   end
   Ignore = Class.new
-
+  ELSE_KEYWORD_FOR_IF = QT_Symbol.new( :else )
   NoRet = Class.new
   FUNCTIONS = {
     QT_Symbol.new( :switch ) => QT_BuiltinFunciton.new{ |args, env|
-      switch_on = fetch(args, 0, :__switch_on)
-      args.qt_get(switch_on, env, type: QT_Symbol.new( :BOTH ))
+      switch_on = fetch(args, env, 0, :__switch_on)
+      cases = fetch(args, env, :__cases, default: args, can_pass_block: true)
+      cases = cases.qt_eval(env) if $BLOCK_GIVEN
+      cases.qt_get(switch_on, env, type: QT_Symbol.new( :BOTH ))
     },
     QT_Symbol.new( :if ) => QT_BuiltinFunciton.new{ |args, env|
-      cond     = fetch(args, 0, :__cond)
-      if_true  = fetch(args, 1, QT_True::INSTANCE , :true)
-      if_false = fetch(args, 2, QT_False::INSTANCE, :false, default: QT_Null::INSTANCE)
-      if_false = args.locals.fetch(false){ args.stack.fetch(2){ args.locals.fetch(:false, QT_Null::INSTANCE) } }
-      cond.qt_to_bool(env).bool_val ? if_true : if_false
+      cond     = fetch(args, env, 0, :__cond)
+      if_true  = fetch(args, env, 1, QT_True::INSTANCE, :true, can_pass_block: true)
+      true_block = $BLOCK_GIVEN
+      if true_block && ELSE_KEYWORD_FOR_IF == env.p.next_token!(env.clone)[0]
+        raise unless ELSE_KEYWORD_FOR_IF == env.p.next_token!(env)[0]
+      end
+      if_false = fetch(args, env, 2, QT_False::INSTANCE, :false, default: QT_Null::INSTANCE, can_pass_block: true)
+      false_block = $BLOCK_GIVEN
+      if cond.qt_to_bool(env).bool_val
+        true_block ? if_true.qt_eval(env) : if_true
+      else
+        false_block ? if_false.qt_eval(env) : if_false
+      end
     },
-    QT_Symbol.new( :unless ) => QT_BuiltinFunciton.new{ |args, env|
-      cond     = fetch(args, 0, :__cond)
-      if_true  = fetch(args, 1, QT_True::INSTANCE , :true)
-      if_false = fetch(args, 2, QT_False::INSTANCE, :false, default: QT_Null::INSTANCE)
-      if_false = args.locals.fetch(false){ args.stack.fetch(2){ args.locals.fetch(:false, QT_Null::INSTANCE) } }
-      !cond.qt_to_bool(env).bool_val ? if_true : if_false
+    QT_Symbol.new( :unless ) => QT_BuiltinFunciton.new{ |args, env| 
+      cond = fetch(args, env, 0, :__cond)
+
+      if_false = fetch(args, env, 1, QT_False::INSTANCE, :false, can_pass_block: false)
+      false_block = $BLOCK_GIVEN
+
+      if false_block && ELSE_KEYWORD_FOR_IF == env.p.next_token!(env.clone)[0]
+        raise unless ELSE_KEYWORD_FOR_IF == env.p.next_token!(env)[0]
+      end
+
+      if_true = fetch(args, env, 2, QT_True::INSTANCE, :true, default: QT_Null::INSTANCE, can_pass_block: false)
+      true_block = $BLOCK_GIVEN
+
+      if cond.qt_to_bool(env).bool_val
+        false_block ? if_false.qt_eval(env) : if_false
+      else
+        true_block ? if_true.qt_eval(env) : if_true
+      end
+
     },
 
     QT_Symbol.new( :while ) => QT_BuiltinFunciton.new{ |args, env|
-      cond = fetch(args, 0, :__cond)
-      body = fetch(args, 1, :__body)
+      cond = fetch(args, env, 0, :__cond)
+      body = fetch(args, env, 1, :__body, can_pass_block: true)
       while cond.clone.qt_eval(env).pop.qt_to_bool(env).bool_val
         body.clone.qt_eval(env)
       end
       QT_Null::INSTANCE
     },
     QT_Symbol.new( :until ) => QT_BuiltinFunciton.new{ |args, env|
-      cond = fetch(args, 0, :__cond)
-      body = fetch(args, 1, :__body)
+      cond = fetch(args, env, 0, :__cond)
+      body = fetch(args, env, 1, :__body, can_pass_block: trues)
       until cond.clone.qt_eval(env).pop.qt_to_bool(env).bool_val
         body.clone.qt_eval(env)
       end
@@ -53,21 +76,11 @@ module Functions
     },
 
     QT_Symbol.new( :for ) => QT_BuiltinFunciton.new{ |args, env|
-      start = fetch(args, 0, :__start)
-      cond =  fetch(args, 1, :__cond)
-      incr =  fetch(args, 2, :__incr)
-      body =  fetch(args, 3, :__body, default: nil)
-      unless body
-        ntoken = env.p.next_token!(env.clone)
-        if Operators::DELIMS.include?(ntoken[0])
-          raise unless Operators::DELIMS.include?(env.p.next_token!(env)[0]) # get rid of the , or ;
-          body = env.p.next_token!(env)[0]
-        end
-      end
-      fail "Cannot find body for `for`" unless body
-      p body
-      # exit
-      # start.clone.qt_eval(env)
+      start = fetch(args, env, 0, :__start)
+      cond =  fetch(args, env, 1, :__cond)
+      incr =  fetch(args, env, 2, :__incr)
+      body =  fetch(args, env, 3, :__body, can_pass_block: true)
+      start.is_a?(QT_Universe) && start.qt_eval(env)
       while cond.clone.qt_eval(env).pop.qt_to_bool(env).bool_val
         body.clone.qt_eval(env)
         incr.clone.qt_eval(env)
@@ -77,22 +90,22 @@ module Functions
 
     QT_Symbol.new( :disp ) => QT_BuiltinFunciton.new{ |args, env|
 
-      endl = fetch(args, :end, default: QT_Text.new("\n"))
-      sep = fetch(args, :sep, default: QT_Text.new(""))
+      endl = fetch(args, env, :end, default: QT_Text.new("\n"))
+      sep = fetch(args, env, :sep, default: QT_Text.new(""))
       to_print = args.stack.collect{ |arg| arg.qt_to_text(env).text_val }.join(sep.qt_to_text(env).text_val)
       print(to_print + endl.qt_to_text(env).text_val)
       QT_Null::INSTANCE
     },
     QT_Symbol.new( :prompt ) => QT_BuiltinFunciton.new{ |args, env|
-      prompt = fetch(args, 0, :__prompt, default: QT_Text.new( '' ))
-      prefix = fetch(args, 1, :__prefix, default: QT_Text.new( " = " ))
-      endl = fetch(args, 2, :__endl, default: QT_Text.new( "\n" ))
+      prompt = fetch(args, env, 0, :__prompt, default: QT_Text.new( '' ))
+      prefix = fetch(args, env, 1, :__prefix, default: QT_Text.new( " = " ))
+      endl = fetch(args, env, 2, :__endl, default: QT_Text.new( "\n" ))
       print prompt.text_val + prefix.text_val
       QT_Text.new( STDIN.gets(endl.text_val).chomp )
     },
 
     QT_Symbol.new( :syscall ) => QT_BuiltinFunciton.new{ |args, env|
-      sep = fetch(args, :sep, default: QT_Text.new(""))
+      sep = fetch(args, env, :sep, default: QT_Text.new(""))
       args.locals[QT_Symbol.new( :sep ) ] ||= sep
       to_call = FUNCTIONS[ QT_Symbol.new( :text ) ].qt_call(args, env) 
       QT_Text.new( `#{to_call}` )
@@ -100,9 +113,9 @@ module Functions
 
 
     QT_Symbol.new( :text ) => QT_BuiltinFunciton.new{ |args, env|
-      to_text = fetch(args, 0, :__to_text, default: QT_Text::EMPTY)
-      quote1   = fetch(args, 1, :quote, :__quote,  :quote1, :__quote1, default: QT_Text.new( '"' ))
-      quote2   = fetch(args, 2, :quote2, :__quote2, default: quote1)
+      to_text = fetch(args, env, 0, :__to_text, default: QT_Text::EMPTY)
+      quote1   = fetch(args, env, 1, :quote, :__quote,  :quote1, :__quote1, default: QT_Text.new( '"' ))
+      quote2   = fetch(args, env, 2, :quote2, :__quote2, default: quote1)
       res = to_text.qt_to_text(env)
       res.quotes = [quote1.qt_to_text(env), 
                     quote2.qt_to_text(env)]
@@ -110,25 +123,25 @@ module Functions
     },
 
     QT_Symbol.new( :num ) => QT_BuiltinFunciton.new{ |args, env|
-      fetch(args, 0, :__to_num, default: QT_Number::ZERO).qt_to_num(env)
+      fetch(args, env, 0, :__to_num, default: QT_Number::ZERO).qt_to_num(env)
     },
 
     QT_Symbol.new( :stop ) => QT_BuiltinFunciton.new{ |args, env|
-      exit fetch(args, 0, :__code, default: QT_Number::ZERO).qt_to_num(env).num_val
+      exit fetch(args, env, 0, :__code, default: QT_Number::ZERO).qt_to_num(env).num_val
     },
 
 
     QT_Symbol.new( :bool ) => QT_BuiltinFunciton.new{ |args, env|
-      fetch(args, 0, :__to_num, default: QT_False::INSTANCE).qt_to_bool(env)
+      fetch(args, env, 0, :__to_num, default: QT_False::INSTANCE).qt_to_bool(env)
     },
 
     QT_Symbol.new( :type ) => QT_BuiltinFunciton.new{ |args, env|
-      fetch(args, 0, :__to_type).qt_to_type(env)
+      fetch(args, env, 0, :__to_type).qt_to_type(env)
     },
 
     QT_Symbol.new( :len ) => QT_BuiltinFunciton.new{ |args, env|
-      arg = fetch(args, 0, :__arg)
-      arg.qt_length( env, type: fetch(args, 1, :__type, default: QT_Symbol.new( :BOTH )))
+      arg = fetch(args, env, 0, :__arg)
+      arg.qt_length( env, type: fetch(args, env, 1, :__type, default: QT_Symbol.new( :BOTH )))
     },
 
 
@@ -184,11 +197,19 @@ module Functions
 
   module_function
 
-  def fetch(args, *search_fors, default: Ignore)
+  def fetch(args, env, *search_fors, default: Ignore, can_pass_block: false)
+    $BLOCK_GIVEN = false
     search_fors.each do |search_for|
       res = (search_for.is_a?(Integer) ? args.stack : args.locals).fetch(
               (search_for.is_a?(Symbol) ? QT_Symbol.new( search_for ) : search_for), Ignore)
       return res unless res.equal?( Ignore )
+    end
+    if can_pass_block
+      ntoken = env.p.next_token!(env.clone)
+      raise unless Operators::DELIMS.include?(env.p.next_token!(env)[0]) if Operators::DELIMS.include?(ntoken[0])
+      # ^^ get rid of the [';' or ','] outside the function.
+      $BLOCK_GIVEN = true
+      return env.p.next_token!(env)[0]
     end
     raise "Cannot find args for #{search_fors}" if default.equal?(Ignore)
     default
--- qutie.rb
+++ qutie.rb
@@ -1,49 +1,60 @@
-require_relative 'environment'
-require_relative 'pre_parser'
-require_relative 'contexts'
-require_relative 'parser'
-require_relative 'exceptions/exception'
-require_relative 'plugins/whitespace/plugin'
-require_relative 'plugins/universe/plugin'
-require_relative 'plugins/number/plugin'
-require_relative 'plugins/operator/plugin'
-require_relative 'plugins/symbol/plugin'
-require_relative 'plugins/comment/plugin'
-require_relative 'plugins/text/plugin'
+# require_relative 'environment'
+# require_relative 'pre_parser'
+# require_relative 'contexts'
+# require_relative 'parser'
+# require_relative 'exceptions/exception'
+# require_relative 'plugins/whitespace/plugin'
+# require_relative 'plugins/universe/plugin'
+# require_relative 'plugins/number/plugin'
+# require_relative 'plugins/operator/plugin'
+# require_relative 'plugins/symbol/plugin'
+# require_relative 'plugins/comment/plugin'
+# require_relative 'plugins/text/plugin'
 
-require_relative 'functions'
-require_relative 'constants'
-parser = Parser.new
-parser.add_plugin(plugin: Whitespace)
-parser.add_plugin(plugin: Symbols)
-parser.add_plugin(plugin: Number)
-parser.add_plugin(plugin: Universe)
-parser.add_plugin(plugin: Operators)
-parser.add_plugin(plugin: Comment)
-parser.add_plugin(plugin: Text)
-parser.add_builtins(builtins: Functions::FUNCTIONS)
-parser.add_builtins(builtins: Constants::CONSTANTS)
-# ARGV[0] = '/Users/westerhack/code/ruby/qutie/examples/users.qt'
-file = ARGV[0] or fail "No file!"
-text = open(file, 'r').read
-PreParser::pre_process!(text)
+# require_relative 'functions'
+# require_relative 'constants'
+# parser = Parser.new
+# parser.add_plugin(plugin: Whitespace)
+# parser.add_plugin(plugin: Symbols)
+# parser.add_plugin(plugin: Number)
+# parser.add_plugin(plugin: Universe)
+# parser.add_plugin(plugin: Operators)
+# parser.add_plugin(plugin: Comment)
+# parser.add_plugin(plugin: Text)
+# parser.add_builtins(builtins: Functions::FUNCTIONS)
+# parser.add_builtins(builtins: Constants::CONSTANTS)
+# # ARGV[0] = '/Users/westerhack/code/ruby/qutie/examples/users.qt'
+# file = ARGV[0] or fail "No file!"
+# text = open(file, 'r').read
+# PreParser::pre_process!(text)
 
-# puts text
-# exit
-$QT_CONTEXT = Contexts.new
-res=nil
-err = catch(:ERROR) do 
-  res = parser.process(input: text)
-  nil
+# # puts text
+# # exit
+# $QT_CONTEXT = Contexts.new
+# res=nil
+# err = catch(:ERROR) do 
+#   res = parser.process(input: text)
+#   nil
+# end
+
+# if err
+#   puts "--ERROR--\n#{err}\n\n"
+#   exit(1)
+# end
+
+# require 'pp'
+# puts '----[end]----'
+# res.u.stack.each_with_index{ |i,j| puts "\t#{j}: #{i.inspect}"}
+# puts '-------------'
+
+unless true
+  print "false"
+else
+  print "true"
 end
 
-if err
-  puts "--ERROR--\n#{err}\n\n"
-  exit(1)
-end
 
-require 'pp'
-puts '----[end]----'
-res.u.stack.each_with_index{ |i,j| puts "\t#{j}: #{i.inspect}"}
-puts '-------------'
+
+
+
 
